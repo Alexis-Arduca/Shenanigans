152764328b3033a4fb0814a4066ef9716facda78
Viro Neo <van-do.pham@epitech.eu>
2025-03-04 16:48:16 +0100

    :sparkles: now I use a brush to paint a texture every frame

--- Assets/Scripts/SphereTrail.cs
+++ Assets/Scripts/SphereTrail.cs
@@ -1,5 +1,35 @@
 using UnityEngine;
+using UnityEngine.TerrainTools;
 
 public class SphereTrail : MonoBehaviour
 {
+    public Material material;
+    public RenderTexture texture;
+    
+    public GameObject brushPrefab;
+    public Transform plane;
+    
+    private void Start()
+    {
+        material.mainTexture = texture;
+    }
+
+    private void Update()
+    {
+        Paint();
+    }
+    
+    private void Paint()
+    {
+        if (!brushPrefab || !plane) return;
+
+        // Instantiate a brush
+        GameObject brush = Instantiate(brushPrefab, transform.position, Quaternion.identity);
+        
+        // Make sure it's aligned with the painting plane
+        brush.transform.position = new Vector3(transform.position.x, plane.position.y + 0.01f, transform.position.z);
+        
+        // // Optionally, destroy after a delay to reduce clutter
+        // Destroy(brush, 2f);
+    }
 }
